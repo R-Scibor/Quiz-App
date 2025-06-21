bd69a98b68e27a241e2ab7046af1d2e7cbb87b8e
Zyrandool <zyrafka22@gmail.com>
2025-06-21 16:32:52 +0200

    feat: define REACT_APP_BUILD_PATH for frontend build directory

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REACT_APP_BUILD_PATH = os.path.join(BASE_DIR, 'frontend/dist')
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
